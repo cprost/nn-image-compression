7cbf8c63d67b3845795f42a73c266fbaa6d90995
Chris Prost <chris.ulo.prost@gmail.com>
2019-08-07 17:52:09 -0400

    Completed full autoencoder, added image printing

--- image_compression.py
+++ image_compression.py
@@ -2,43 +2,118 @@ import image_preprocessor
 import tensorflow as tf
 import numpy as np
 import cv2
+import time
 from tensorflow import keras
 
+'''
+Run this file to download the MNIST/CIFAR10 dataset(s) and prepare them for the neural network.
+
+The neural network architecture is specified in 'model', and takes 'vec_len' and 'bottleneck'
+as variable parameters. The 'bottleneck' value determines the degree of compression in the
+autoencoder.
+
+Predicting any sample can be tested with 'predict_output(<SAMPLE #>)'
+'''
+
+def image_grid(img_data, indices, suffix):
+    dims = img_data[0].shape
+
+    grid = np.ones((dims[0], 10))
+    grid = cv2.normalize(grid, None, 255, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    buffer = grid.copy()
+
+    for i in indices:
+        current = cv2.normalize(img_data[i], None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+        grid = np.concatenate((grid, current), axis=1)
+        grid = np.concatenate((grid, buffer), axis=1)
+
+    currdate = time.strftime("%Y%m%d_%H%M%S")
+    filename = 'autoencoder_' + currdate + "_" + suffix + '.png'
+
+    grid = grid.astype('uint8')
+    cv2.imwrite(filename, grid)
+
+def unflatten(img_data, dims):
+    samples = len(img_data)
+    new_data = img_data.reshape(samples, dims[1], dims[0]) # vector to original image size
+    return new_data
+
+def predict_output(idx):
+    input = input_data[idx]
+    input = input.reshape(1, vec_len) # vector to original image size
+    output = model.predict(input)
+    output = output.reshape(dims[1], dims[0])
+    input = input.reshape(dims[1], dims[0])
+    cv2.imshow('Original Image at Index = ' + str(idx), input)
+    cv2.imshow('Reproduced Image at Index = ' + str(idx), output)
+    cv2.waitKey(0)
+
+def sample_output(idx):
+    encoded_out = compressor.predict(input_data[idx].reshape(1, vec_len)) # vector to original image size
+    decoded_out = decoder.predict(encoded_out)
+    decoded_out = decoded_out.reshape(dims[1], dims[0])
+    cv2.imshow('Sample of separated encoder-decoder results', decoded_out)
+    cv2.waitKey(0)
+
+# NEURAL NETWORK BEGINS BELOW
+
 if __name__ == '__main__':
-    input_data = image_preprocessor.generate_data()
+    input_data, samples, dims = image_preprocessor.generate_data()
 
-samples = input_data.shape[0]
-vec_len = input_data.shape[1]
+# saved input_data can also be imported using 'input_data = np.load('cleaned_<DATASET>_data.npy')
 
+vec_len = dims[0] * dims[1]
+bottleneck = 512  # size of the bottleneck layer in neurons
 
 model = keras.Sequential([
-    keras.layers.Dense(vec_len, input_dim=vec_len),
+    keras.layers.Dense(vec_len, activation=tf.nn.relu),
     keras.layers.Dense(256, activation=tf.nn.relu),
-    keras.layers.Dense(192, activation=tf.nn.relu),  # bottleneck stage
+    keras.layers.Dense(bottleneck, activation=tf.nn.relu),  # bottleneck stage
     keras.layers.Dense(256, activation=tf.nn.relu),
     keras.layers.Dense(vec_len, activation=tf.nn.relu)
 ])
 
-model.compile(optimizer='adam', loss=tf.keras.losses.CategoricalCrossentropy())
-model.fit(input_data, input_data, verbose=1, epochs=10, batch_size=256)
+model.compile(optimizer='adam', loss='mse')
+model.fit(input_data, input_data, verbose=1, epochs=1, batch_size=256)
 
 compressor = keras.Sequential([
-    keras.layers.Dense(vec_len, input_dim=vec_len, weights=model.layers[0].get_weights()),
+    keras.layers.Dense(vec_len, activation=tf.nn.relu, weights=model.layers[0].get_weights()),
     keras.layers.Dense(256, activation=tf.nn.relu, weights=model.layers[1].get_weights()),
-    keras.layers.Dense(192, activation=tf.nn.relu, weights=model.layers[2].get_weights())
+    keras.layers.Dense(bottleneck, activation=tf.nn.relu, weights=model.layers[2].get_weights())
 ])
 
-compressor.compile(optimizer='adam', loss=tf.keras.losses.CategoricalCrossentropy())
+decoder = keras.Sequential([
+    keras.layers.Dense(256, activation=tf.nn.relu, input_dim=bottleneck, weights=model.layers[3].get_weights()),
+    keras.layers.Dense(vec_len, activation=tf.nn.relu, weights=model.layers[4].get_weights())
+])
 
-test_out = compressor.predict(input_data[172].reshape(1, vec_len))  # should be 128x1 output
-print(test_out)
-
-test = input_data[172].reshape(1, vec_len)  # random sample
-y_test = model.predict(test)
-
-test = np.reshape(test, (-1, 28))  # change 28 to image dims
-y_test = np.reshape(y_test, (-1, 28))  # change 28 to image dims
-
-cv2.imshow('Test Image', test)
-cv2.imshow('Output Image', y_test)
+mytest = compressor.predict(input_data[5].reshape(1, vec_len))
+myout = decoder.predict(mytest)
+myout = myout.reshape(dims[1], dims[0])
+cv2.imshow('el', myout)
 cv2.waitKey(0)
+
+compressor.compile(optimizer='adam', loss='mse')
+decoder.compile(optimizer='adam', loss='mse')
+
+sample_idx = [283, 277, 272, 299, 295] # sample indices of even numbers in MNIST, otherwise random
+sample_in = input_data[sample_idx]
+sample_in = sample_in.reshape(len(sample_idx), 1, vec_len)
+
+output_data = model.predict(sample_in[0])
+for i in range(1, len(sample_idx)):
+    output_data = np.append(output_data, model.predict(sample_in[i]), axis=0)
+
+output_data = output_data.reshape(len(sample_idx), 1, vec_len)
+
+# restores flattened vectors to original image shape
+restored_input = unflatten(input_data, dims)
+restored_output = unflatten(output_data, dims)
+
+# prints image grids to file
+image_grid(restored_input, sample_idx, 'in')
+image_grid(restored_output, np.arange(len(sample_idx)), 'out')
+
+# sample prediction at mysample = <SAMPLE #> in dataset
+# mysample = 999
+# predict_output(mysample)
--- image_preprocessor.py
+++ image_preprocessor.py
@@ -1,22 +1,25 @@
 import tensorflow as tf
 import numpy as np
 
+'''
+RUN 'image_compression.py' INSTEAD TO SEE THE START-TO-FINISH AUTOENCODER
 
+Running this script will only download a dataset, clean and preprocess it, and save it as a .npy file
+'''
 
-def grayscale(data, dtype='float32'):  # luma coding
+def grayscale(data, dtype='float32'):
+    # luma coding, see https://en.wikipedia.org/wiki/Grayscale#Luma_coding_in_video_systems
     r, g, b = np.asarray(.299, dtype=dtype), np.asarray(.587, dtype=dtype), np.asarray(.114, dtype=dtype)
-    rst = r * data[:, :, :, 0] + g * data[:, :, :, 1] + b * data[:, :, :, 2]
-    rst = np.expand_dims(rst, axis=3)
-    return rst
+    luma = r * data[:, :, :, 0] + g * data[:, :, :, 1] + b * data[:, :, :, 2]
+    luma = np.expand_dims(luma, axis=3)
+    return luma
 
 def generate_data():
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     # can use mnist, cifar10 datasets from the keras repository
     # we only really need the x_train variable, the rest can be ignored
 
     print("Using TensorFlow ver.", tf.__version__)
-
-    print(x_train.shape)
 
     # TF requires 4D tensor in order: (samples, rows, cols, channels)
     samples = x_train.shape[0]
@@ -27,8 +30,6 @@ def generate_data():
         channels = x_train.shape[3]
         if channels != 1:
             x_train = grayscale(x_train)
-
-    print(x_train.shape)
 
     print("Imported", samples, image_cols, "x", image_rows, "images.")
 
@@ -48,4 +49,13 @@ def generate_data():
     x_train = x_train.astype('float32') # for compatibility with tf
     x_train /= 255  # normalize
 
-    return x_train
+    return x_train, samples, (image_rows, image_cols)
+
+if __name__ == '__main__':
+    print("NOTE: To train or use the autoencoder, run 'image_compression.py' instead.")
+    print("Running this script will only save the preprocessed data.\n")
+    print("Please select the dataset in 'generate_data()'\n")
+
+    input_data, samples, dims = generate_data()
+
+    np.save('cleaned_input_data.npy', input_data)
